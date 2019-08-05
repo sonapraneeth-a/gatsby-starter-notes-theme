7e36749f11b31ad3dded2a469b2a0f549fc62b20
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-08-05 23:19:14 +0530

    chore: added siddharth components

--- src/gatsby-theme-notes/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-theme-notes/gatsby-plugin-theme-ui/components.js
@@ -1,7 +1,12 @@
 // src/gatsby-plugin-theme-ui/components.js
 import Prism from '@theme-ui/prism'
  
+const MyH1 = props => <h1 style={{ color: "tomato" }} {...props} />
+const MyParagraph = props => <p style={{ fontSize: "18px", lineHeight: 1.6 }} />
+
 export default {
   pre: props => props.children,
   code: Prism,
+  h1: MyH1,
+  p: MyParagraph,
 }
